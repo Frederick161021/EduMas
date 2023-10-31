b97f12bf46f9f6ef8ad55782db26eb6723b88895
Frederick <togaeriv01@gmail.com>
2023-10-30 18:15:35 -0600

    pantalla resultados

--- CursoEd/Resultado.Designer.vb
+++ CursoEd/Resultado.Designer.vb
@@ -22,9 +22,63 @@ Partial Class Resultado
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Resultado))
+        Me.btnSalirEsp1 = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.lblResultado = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnSalirEsp1
+        '
+        Me.btnSalirEsp1.Font = New System.Drawing.Font("Impact", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSalirEsp1.Location = New System.Drawing.Point(274, 242)
+        Me.btnSalirEsp1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.btnSalirEsp1.Name = "btnSalirEsp1"
+        Me.btnSalirEsp1.Size = New System.Drawing.Size(126, 32)
+        Me.btnSalirEsp1.TabIndex = 38
+        Me.btnSalirEsp1.Text = "Salír"
+        Me.btnSalirEsp1.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(-12, -2)
+        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(717, 369)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 37
+        Me.PictureBox1.TabStop = False
+        '
+        'lblResultado
+        '
+        Me.lblResultado.AutoSize = True
+        Me.lblResultado.BackColor = System.Drawing.Color.Transparent
+        Me.lblResultado.Font = New System.Drawing.Font("Microsoft YaHei", 13.8!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblResultado.Location = New System.Drawing.Point(283, 165)
+        Me.lblResultado.Name = "lblResultado"
+        Me.lblResultado.Size = New System.Drawing.Size(128, 31)
+        Me.lblResultado.TabIndex = 39
+        Me.lblResultado.Text = "Resultado"
+        '
+        'Resultado
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(701, 365)
+        Me.Controls.Add(Me.lblResultado)
+        Me.Controls.Add(Me.btnSalirEsp1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Resultado"
         Me.Text = "Resultado"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+
+    Friend WithEvents btnSalirEsp1 As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents lblResultado As Label
 End Class
--- CursoEd/Resultado.vb
+++ CursoEd/Resultado.vb
@@ -1,3 +1,6 @@
 ﻿Public Class Resultado
-
+    Private Sub btnSalirEsp1_Click(sender As Object, e As EventArgs) Handles btnSalirEsp1.Click
+        Cursos.Show()
+        Me.Hide()
+    End Sub
 End Class
