04b7eb1a30cb3310a8ee4ff7580e1dcdbf4451b5
GabrielaPPP13 <cngabriela20@gmail.com>
2023-11-02 16:06:38 -0600

    Agregados archivos CursoEd/obj/Debug/CursoEd.adminActividad.resources, CursoEd/obj/Debug/CursoEd.adminPago.resources, CursoEd/adminPago.Designer.vb, CursoEd/adminActividad.vb

--- CursoEd/adminPago.Designer.vb
+++ CursoEd/adminPago.Designer.vb
@@ -25,21 +25,25 @@ Partial Class adminPago
         Me.btnBuscar = New System.Windows.Forms.Button()
         Me.lblUsuario = New System.Windows.Forms.Label()
         Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.txtCurso_cost = New System.Windows.Forms.TextBox()
         Me.btnEliminar = New System.Windows.Forms.Button()
-        Me.btnActualizar = New System.Windows.Forms.Button()
-        Me.btnAgregar = New System.Windows.Forms.Button()
         Me.Label8 = New System.Windows.Forms.Label()
         Me.Label7 = New System.Windows.Forms.Label()
         Me.Label6 = New System.Windows.Forms.Label()
         Me.Label5 = New System.Windows.Forms.Label()
         Me.Label4 = New System.Windows.Forms.Label()
-        Me.txtCurso_cost = New System.Windows.Forms.TextBox()
+        Me.txtPago_fecha = New System.Windows.Forms.TextBox()
         Me.Label3 = New System.Windows.Forms.Label()
-        Me.txtCurso_descripcion = New System.Windows.Forms.TextBox()
-        Me.txtCurso_nombre = New System.Windows.Forms.TextBox()
+        Me.txtPago_curso_id = New System.Windows.Forms.TextBox()
+        Me.txtPago_usuario_id = New System.Windows.Forms.TextBox()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.txtCurso_id = New System.Windows.Forms.TextBox()
+        Me.txtPago_id = New System.Windows.Forms.TextBox()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.txtPago_descuento = New System.Windows.Forms.TextBox()
+        Me.btnLimpiar = New System.Windows.Forms.Button()
+        Me.btnRegresar = New System.Windows.Forms.Button()
         Me.Panel1.SuspendLayout()
         Me.SuspendLayout()
         '
@@ -71,22 +75,44 @@ Partial Class adminPago
         'Panel1
         '
         Me.Panel1.BackgroundImage = Global.CursoEd.My.Resources.Resources.fondo1
+        Me.Panel1.Controls.Add(Me.btnLimpiar)
+        Me.Panel1.Controls.Add(Me.Label10)
+        Me.Panel1.Controls.Add(Me.txtPago_descuento)
+        Me.Panel1.Controls.Add(Me.Label9)
+        Me.Panel1.Controls.Add(Me.txtCurso_cost)
         Me.Panel1.Controls.Add(Me.btnEliminar)
-        Me.Panel1.Controls.Add(Me.btnActualizar)
-        Me.Panel1.Controls.Add(Me.btnAgregar)
         Me.Panel1.Controls.Add(Me.Label8)
         Me.Panel1.Controls.Add(Me.Label7)
         Me.Panel1.Controls.Add(Me.Label6)
         Me.Panel1.Controls.Add(Me.Label5)
         Me.Panel1.Controls.Add(Me.Label4)
-        Me.Panel1.Controls.Add(Me.txtCurso_cost)
+        Me.Panel1.Controls.Add(Me.txtPago_fecha)
         Me.Panel1.Controls.Add(Me.Label3)
-        Me.Panel1.Controls.Add(Me.txtCurso_descripcion)
-        Me.Panel1.Controls.Add(Me.txtCurso_nombre)
+        Me.Panel1.Controls.Add(Me.txtPago_curso_id)
+        Me.Panel1.Controls.Add(Me.txtPago_usuario_id)
         Me.Panel1.Location = New System.Drawing.Point(29, 122)
         Me.Panel1.Name = "Panel1"
         Me.Panel1.Size = New System.Drawing.Size(743, 299)
         Me.Panel1.TabIndex = 23
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.BackColor = System.Drawing.Color.Transparent
+        Me.Label9.Font = New System.Drawing.Font("Microsoft YaHei", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(106, 156)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(54, 21)
+        Me.Label9.TabIndex = 12
+        Me.Label9.Text = "Costo"
+        '
+        'txtCurso_cost
+        '
+        Me.txtCurso_cost.Font = New System.Drawing.Font("Microsoft YaHei", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtCurso_cost.Location = New System.Drawing.Point(171, 156)
+        Me.txtCurso_cost.Name = "txtCurso_cost"
+        Me.txtCurso_cost.Size = New System.Drawing.Size(533, 29)
+        Me.txtCurso_cost.TabIndex = 11
         '
         'btnEliminar
         '
@@ -94,40 +120,12 @@ Partial Class adminPago
         Me.btnEliminar.FlatStyle = System.Windows.Forms.FlatStyle.Popup
         Me.btnEliminar.Font = New System.Drawing.Font("Microsoft YaHei", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btnEliminar.ForeColor = System.Drawing.Color.White
-        Me.btnEliminar.Location = New System.Drawing.Point(249, 248)
+        Me.btnEliminar.Location = New System.Drawing.Point(30, 250)
         Me.btnEliminar.Name = "btnEliminar"
-        Me.btnEliminar.Size = New System.Drawing.Size(273, 29)
+        Me.btnEliminar.Size = New System.Drawing.Size(273, 34)
         Me.btnEliminar.TabIndex = 10
         Me.btnEliminar.Text = "Eliminar"
         Me.btnEliminar.UseVisualStyleBackColor = True
-        '
-        'btnActualizar
-        '
-        Me.btnActualizar.BackgroundImage = Global.CursoEd.My.Resources.Resources.fondo4
-        Me.btnActualizar.FlatStyle = System.Windows.Forms.FlatStyle.Popup
-        Me.btnActualizar.Font = New System.Drawing.Font("Microsoft YaHei", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnActualizar.ForeColor = System.Drawing.Color.White
-        Me.btnActualizar.Location = New System.Drawing.Point(431, 196)
-        Me.btnActualizar.Name = "btnActualizar"
-        Me.btnActualizar.Size = New System.Drawing.Size(273, 29)
-        Me.btnActualizar.TabIndex = 9
-        Me.btnActualizar.Text = "Actualizar"
-        Me.btnActualizar.UseVisualStyleBackColor = True
-        '
-        'btnAgregar
-        '
-        Me.btnAgregar.BackColor = System.Drawing.Color.Transparent
-        Me.btnAgregar.BackgroundImage = Global.CursoEd.My.Resources.Resources.fondo4
-        Me.btnAgregar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
-        Me.btnAgregar.FlatStyle = System.Windows.Forms.FlatStyle.Popup
-        Me.btnAgregar.Font = New System.Drawing.Font("Microsoft YaHei", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnAgregar.ForeColor = System.Drawing.Color.White
-        Me.btnAgregar.Location = New System.Drawing.Point(47, 196)
-        Me.btnAgregar.Name = "btnAgregar"
-        Me.btnAgregar.Size = New System.Drawing.Size(273, 29)
-        Me.btnAgregar.TabIndex = 8
-        Me.btnAgregar.Text = "Agregar"
-        Me.btnAgregar.UseVisualStyleBackColor = False
         '
         'Label8
         '
@@ -164,31 +162,31 @@ Partial Class adminPago
         Me.Label5.AutoSize = True
         Me.Label5.BackColor = System.Drawing.Color.Transparent
         Me.Label5.Font = New System.Drawing.Font("Microsoft YaHei", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label5.Location = New System.Drawing.Point(106, 110)
+        Me.Label5.Location = New System.Drawing.Point(81, 69)
         Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(54, 21)
+        Me.Label5.Size = New System.Drawing.Size(74, 21)
         Me.Label5.TabIndex = 7
-        Me.Label5.Text = "Costo"
+        Me.Label5.Text = "Id Curso"
         '
         'Label4
         '
         Me.Label4.AutoSize = True
         Me.Label4.BackColor = System.Drawing.Color.Transparent
         Me.Label4.Font = New System.Drawing.Font("Microsoft YaHei", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label4.Location = New System.Drawing.Point(104, 69)
+        Me.Label4.Location = New System.Drawing.Point(104, 113)
         Me.Label4.Name = "Label4"
         Me.Label4.RightToLeft = System.Windows.Forms.RightToLeft.Yes
         Me.Label4.Size = New System.Drawing.Size(55, 21)
         Me.Label4.TabIndex = 7
         Me.Label4.Text = "Fecha"
         '
-        'txtCurso_cost
+        'txtPago_fecha
         '
-        Me.txtCurso_cost.Font = New System.Drawing.Font("Microsoft YaHei", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtCurso_cost.Location = New System.Drawing.Point(171, 110)
-        Me.txtCurso_cost.Name = "txtCurso_cost"
-        Me.txtCurso_cost.Size = New System.Drawing.Size(533, 29)
-        Me.txtCurso_cost.TabIndex = 6
+        Me.txtPago_fecha.Font = New System.Drawing.Font("Microsoft YaHei", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtPago_fecha.Location = New System.Drawing.Point(171, 110)
+        Me.txtPago_fecha.Name = "txtPago_fecha"
+        Me.txtPago_fecha.Size = New System.Drawing.Size(533, 29)
+        Me.txtPago_fecha.TabIndex = 6
         '
         'Label3
         '
@@ -203,21 +201,21 @@ Partial Class adminPago
         Me.Label3.Text = "Id Usuario"
         Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
-        'txtCurso_descripcion
+        'txtPago_curso_id
         '
-        Me.txtCurso_descripcion.Font = New System.Drawing.Font("Microsoft YaHei", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtCurso_descripcion.Location = New System.Drawing.Point(171, 66)
-        Me.txtCurso_descripcion.Name = "txtCurso_descripcion"
-        Me.txtCurso_descripcion.Size = New System.Drawing.Size(533, 29)
-        Me.txtCurso_descripcion.TabIndex = 6
+        Me.txtPago_curso_id.Font = New System.Drawing.Font("Microsoft YaHei", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtPago_curso_id.Location = New System.Drawing.Point(171, 66)
+        Me.txtPago_curso_id.Name = "txtPago_curso_id"
+        Me.txtPago_curso_id.Size = New System.Drawing.Size(533, 29)
+        Me.txtPago_curso_id.TabIndex = 6
         '
-        'txtCurso_nombre
+        'txtPago_usuario_id
         '
-        Me.txtCurso_nombre.Font = New System.Drawing.Font("Microsoft YaHei", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtCurso_nombre.Location = New System.Drawing.Point(171, 22)
-        Me.txtCurso_nombre.Name = "txtCurso_nombre"
-        Me.txtCurso_nombre.Size = New System.Drawing.Size(533, 29)
-        Me.txtCurso_nombre.TabIndex = 3
+        Me.txtPago_usuario_id.Font = New System.Drawing.Font("Microsoft YaHei", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtPago_usuario_id.Location = New System.Drawing.Point(171, 22)
+        Me.txtPago_usuario_id.Name = "txtPago_usuario_id"
+        Me.txtPago_usuario_id.Size = New System.Drawing.Size(533, 29)
+        Me.txtPago_usuario_id.TabIndex = 3
         '
         'Label2
         '
@@ -241,13 +239,58 @@ Partial Class adminPago
         Me.Label1.TabIndex = 21
         Me.Label1.Text = "Bienvenido: "
         '
-        'txtCurso_id
+        'txtPago_id
         '
-        Me.txtCurso_id.Font = New System.Drawing.Font("Microsoft YaHei", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtCurso_id.Location = New System.Drawing.Point(130, 70)
-        Me.txtCurso_id.Name = "txtCurso_id"
-        Me.txtCurso_id.Size = New System.Drawing.Size(157, 29)
-        Me.txtCurso_id.TabIndex = 20
+        Me.txtPago_id.Font = New System.Drawing.Font("Microsoft YaHei", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtPago_id.Location = New System.Drawing.Point(130, 70)
+        Me.txtPago_id.Name = "txtPago_id"
+        Me.txtPago_id.Size = New System.Drawing.Size(157, 29)
+        Me.txtPago_id.TabIndex = 20
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.BackColor = System.Drawing.Color.Transparent
+        Me.Label10.Font = New System.Drawing.Font("Microsoft YaHei", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label10.Location = New System.Drawing.Point(72, 204)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(91, 21)
+        Me.Label10.TabIndex = 14
+        Me.Label10.Text = "Descuento"
+        '
+        'txtPago_descuento
+        '
+        Me.txtPago_descuento.Font = New System.Drawing.Font("Microsoft YaHei", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtPago_descuento.Location = New System.Drawing.Point(171, 204)
+        Me.txtPago_descuento.Name = "txtPago_descuento"
+        Me.txtPago_descuento.Size = New System.Drawing.Size(533, 29)
+        Me.txtPago_descuento.TabIndex = 13
+        '
+        'btnLimpiar
+        '
+        Me.btnLimpiar.BackgroundImage = Global.CursoEd.My.Resources.Resources.fondo4
+        Me.btnLimpiar.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnLimpiar.Font = New System.Drawing.Font("Microsoft YaHei", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnLimpiar.ForeColor = System.Drawing.Color.White
+        Me.btnLimpiar.Location = New System.Drawing.Point(414, 250)
+        Me.btnLimpiar.Name = "btnLimpiar"
+        Me.btnLimpiar.Size = New System.Drawing.Size(273, 34)
+        Me.btnLimpiar.TabIndex = 15
+        Me.btnLimpiar.Text = "Limpiar"
+        Me.btnLimpiar.UseVisualStyleBackColor = True
+        '
+        'btnRegresar
+        '
+        Me.btnRegresar.BackgroundImage = Global.CursoEd.My.Resources.Resources.fondo3
+        Me.btnRegresar.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnRegresar.Font = New System.Drawing.Font("Microsoft YaHei", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnRegresar.ForeColor = System.Drawing.Color.White
+        Me.btnRegresar.Location = New System.Drawing.Point(633, 12)
+        Me.btnRegresar.Name = "btnRegresar"
+        Me.btnRegresar.Size = New System.Drawing.Size(139, 29)
+        Me.btnRegresar.TabIndex = 26
+        Me.btnRegresar.Text = "Regresar"
+        Me.btnRegresar.UseVisualStyleBackColor = True
         '
         'adminPago
         '
@@ -255,12 +298,13 @@ Partial Class adminPago
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackgroundImage = Global.CursoEd.My.Resources.Resources.fondo2
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnRegresar)
         Me.Controls.Add(Me.btnBuscar)
         Me.Controls.Add(Me.lblUsuario)
         Me.Controls.Add(Me.Panel1)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.txtCurso_id)
+        Me.Controls.Add(Me.txtPago_id)
         Me.Name = "adminPago"
         Me.Text = "adminPago"
         Me.Panel1.ResumeLayout(False)
@@ -274,18 +318,22 @@ Partial Class adminPago
     Friend WithEvents lblUsuario As Label
     Friend WithEvents Panel1 As Panel
     Friend WithEvents btnEliminar As Button
-    Friend WithEvents btnActualizar As Button
-    Friend WithEvents btnAgregar As Button
     Friend WithEvents Label8 As Label
     Friend WithEvents Label7 As Label
     Friend WithEvents Label6 As Label
     Friend WithEvents Label5 As Label
     Friend WithEvents Label4 As Label
-    Friend WithEvents txtCurso_cost As TextBox
+    Friend WithEvents txtPago_fecha As TextBox
     Friend WithEvents Label3 As Label
-    Friend WithEvents txtCurso_descripcion As TextBox
-    Friend WithEvents txtCurso_nombre As TextBox
+    Friend WithEvents txtPago_curso_id As TextBox
+    Friend WithEvents txtPago_usuario_id As TextBox
     Friend WithEvents Label2 As Label
     Friend WithEvents Label1 As Label
-    Friend WithEvents txtCurso_id As TextBox
+    Friend WithEvents txtPago_id As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents txtCurso_cost As TextBox
+    Friend WithEvents Label10 As Label
+    Friend WithEvents txtPago_descuento As TextBox
+    Friend WithEvents btnLimpiar As Button
+    Friend WithEvents btnRegresar As Button
 End Class
